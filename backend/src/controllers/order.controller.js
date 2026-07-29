b218ae49e966e4d6e1d5b5409992497caf398ef9
nayan <205970821+nayan982@users.noreply.github.com>
2026-07-29 14:37:19 +0530

    feat(order): add order status update feature

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -42,3 +42,51 @@ export const getAllOrders = async (req, res) => {
 
   }
 };
+
+export const updateOrderStatus = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const allowedStatus = [
+      "Pending",
+      "Confirmed",
+      "Shipped",
+      "Delivered",
+      "Cancelled",
+    ];
+
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid order status",
+      });
+    }
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        message: "Order not found",
+      });
+    }
+
+    order.status = status;
+
+    await order.save();
+
+    return res.status(200).json({
+      message: "Order status updated successfully",
+      order,
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+
+  }
+};
--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
-import { getAllOrders, getMyOrders } from "../controllers/order.controller.js";
+import { getAllOrders, getMyOrders, updateOrderStatus } from "../controllers/order.controller.js";
 import adminMiddleware from "../middleware/admin.middleware.js";
 
 const router = express.Router();
 
 router.get("/my-orders", authMiddleware, getMyOrders);
 router.get("/", authMiddleware, adminMiddleware, getAllOrders);
+router.patch("/:id/status", authMiddleware, adminMiddleware, updateOrderStatus);
 
 export default router;
