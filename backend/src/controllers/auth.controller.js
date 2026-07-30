bea59d4d732004d70ab04acd80e2926cc0e5c308
nayan <205970821+nayan982@users.noreply.github.com>
2026-07-30 18:22:22 +0530

    update loginuser controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -104,7 +104,7 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
